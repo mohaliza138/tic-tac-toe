6f8180b9a238c3220b566633ab32732af93ceabb
Mohammad Alizadeh <mohaliza138@gmail.com>
2024-06-09 16:02:00 +0330

    init
    Initializing

---
+++ App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import TicTacToe from "./tic-tact-toe";
+
+function App() {
+  return (
+    <div className="App">
+
+      Tic tac toe
+      <TicTacToe/>
+
+    </div>
+  );
+}
+
+export default App;
